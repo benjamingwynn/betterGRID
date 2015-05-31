a53ca36c27639851af801748ba5ec11f64dccb7f
Benjamin Gwynn <xenxier@gmail.com>
2015-05-31 23:10:38 +0100

    Add new icon

--- GRIDfan/My Project/AssemblyInfo.vb
+++ GRIDfan/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("f50e89d1-b5f7-4258-949f-8f5feb3be4b8")> 
+<Assembly: Guid("f50e89d1-b5f7-4258-949f-8f5feb3be4b9")> 
 
 ' Version information for an assembly consists of the following four values:
 '
